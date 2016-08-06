3dca5feb8261c98be2569a7371c35d863804a075
Heather Wolford <goteamwolford@gmail.com>
2016-08-06 18:52:26 -0400

    Set up going well so far.  Hard to remember after working in Java for a while now, but is coming back.

---
+++ CPMD_Week2/CPMD_Week2/FormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormViewController.swift
+//  CPMD_Week2
+//
+//  Created by Heather Wolford on 8/6/16.
+//  Copyright © 2016 Heather Wolford. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
