d7d1fa017151543512aba554e766c21de88bacb4
Heather Wolford <goteamwolford@gmail.com>
2016-08-07 15:03:59 -0400

    All navigation is set up.  UI is complete.  Need to add the Firebase functionality.

---
+++ CPMD_Week2/CPMD_Week2/CreateAccountViewController.swift
@@ -0,0 +1,56 @@
+//
+//  CreateAccountViewController.swift
+//  CPMD_Week2
+//
+//  Created by Heather Wolford on 8/7/16.
+//  Copyright © 2016 Heather Wolford. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadUI()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadUI(){
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(FormViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
